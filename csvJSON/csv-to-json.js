f9a2ae04ba39c67418312a87a8da2c0dd9c30b3d
Awful-Risky <iron.lake@gmail.com>
2016-02-18 14:13:54 -0700

    csvJSON

--- csvJSON/csv-to-json.js
+++ csvJSON/csv-to-json.js
@@ -5,7 +5,6 @@ var sourceFile = process.argv[2];
 var destinationFile = process.argv[3];
 
 var inputCSV = fs.readFileSync(sourceFile, 'utf-8');
-
 var array = inputCSV.split('\n')
 array.pop()
 
@@ -14,16 +13,41 @@ for (var i = 0; i<array.length; i++) {
 	var now = array[i].split(',');
 	newArray.push(now)
 } 
-console.log('newArray: ', newArray)
 
-// function MakeObj (newArray) {
-// 	for (var i = 0; i<newArray.length; i++) {
+var makeObj = function(newArray) {
+	var workArray = [] 
+	for (var j = 1; j<=newArray[0].length; j++) {  //Assumes the first row are the column titles		
+		for (var i = 0; i<newArray[0].length; i++) {			
+			var key = newArray[0][i]
+			var value = newArray[j][i]
+			workArray.push(key + ' : ' + value)
+		}
+		console.log(workArray)	
+	}
+	return workArray;
+}
 
-// 	}
-// }
-// var newObj = makeObj(newArray);
-// console.log(newObj);
+var json = JSON.stringify(makeObj(newArray));
+// console.log(objForJSON);
 
 
-// fs.writeFileSync(destinationFile, newArray);
+
+fs.writeFileSync(destinationFile, json);
+
+// [
+// {
+// 	"name": "Rene", 
+// 	"age": "35", 
+// 	"city": "Vancouver" 
+// },
+// {
+// 	"name": "Thomas", 
+// 	"age": "29", 
+// 	"city": "Portland" 
+// },
+// {
+// 	"name": "Lexi", 
+// 	"age": "28", 
+// 	"city": "Seattle" 
+// }]
 
